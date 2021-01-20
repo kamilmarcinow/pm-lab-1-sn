8aaf72c20ab4d3d0252c425a572811e5fb747639
kamilmarcinow <kamil.marcinowski@student.wat.edu.pl>
2021-01-20 22:16:49 +0100

    "Zakończono zadanie 2.2.3"

--- Zadanie-2-2/src/main.cpp
+++ Zadanie-2-2/src/main.cpp
@@ -1,5 +1,9 @@
 #include <Arduino.h>
 #define LED_RED 10
+#define M_KROPKA_T 200 
+#define M_KRESKA_T 3 * M_KROPKA_T 
+#define M_PAUZA_T 3 * M_KROPKA_T
+#define M_SPACJA_T 7 * M_KROPKA_T
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_RED, OUTPUT);
@@ -8,8 +12,26 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  for (int i = 0; i < 3; i++) 
+{
   digitalWrite(LED_RED, HIGH);
-  delay(500);
+  delay(M_KRESKA_T);
   digitalWrite(LED_RED, LOW);
-  delay(500);
+  delay(M_PAUZA_T);
 }
+for (int i = 0; i < 3; i++)
+{
+  digitalWrite(LED_RED, HIGH);
+  delay(M_KRESKA_T);
+  digitalWrite(LED_RED, LOW);
+  delay(M_PAUZA_T);
+  }
+for (int i = 0; i < 3; i++)
+{
+  digitalWrite(LED_RED, HIGH);
+  delay(M_KRESKA_T);
+  digitalWrite(LED_RED, LOW);
+  delay(M_PAUZA_T);
+  }
+  delay(M_SPACJA_T);
+   }
